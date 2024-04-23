245efa61f3fd182775245a8eb219263bf8ab93a2
deepikathoughts <badiginchaladeepika@gmail.com>
2024-04-23 22:04:40 +0530

    first commit

---
+++ src/demo.java
@@ -0,0 +1,11 @@
+import first.ChromeDriver;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+
+	}
+
+}
